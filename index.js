7fad44c2e493fd75039382031460250b732fe4f5
ritika-saha <ritika882001@gmail.com>
2023-09-23 19:52:32 +0530

    env

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
-
+const port = process.env.PORT || 3000
 // Define routes and application logic here
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const cors = require("cors")
+app.use(cors())
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  })
+  
+  app.use(express.json());
+
+  
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
 });
 
 
